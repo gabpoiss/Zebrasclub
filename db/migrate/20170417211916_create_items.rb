98321116098e836aa9fd6323af2bfd95964f951d
gabriel poissant <poissantgabriel@gmail.com>
2017-04-17 17:34:32 -0400

    Created the full migrations

---
+++ db/migrate/20170417211916_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :items do |t|
+      t.string :size
+      t.integer :stock
+      t.integer :price
+      t.string :brand
+      t.references :category, foreign_key: true
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20170417212651_create_order_items.rb
@@ -0,0 +1,13 @@
+class CreateOrderItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :order_items do |t|
+      t.references :order, foreign_key: true
+      t.references :item, foreign_key: true
+      t.boolean :completed
+      t.integer :quantity
+      t.string :shipping_status
+
+      t.timestamps
+    end
+  end
+end
