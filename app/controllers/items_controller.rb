307baf1bf564563df67480b5ed6a11af8a2a944e
Martin Giannakopoulos <martingianna@gmail.com>
2017-04-18 15:42:02 -0400

    finished rudimentary in-package AJAX item index with filter

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,37 @@ class ItemsController < ApplicationController
   end
 
   def package_index
+
+    if params[:filter]
+      @category_id = Category.where(item_type: params[:category])[0].id
+      @min_price = if params[:min_price] == ""
+        0
+      else
+        params[:min_price].to_i * 100
+      end
+      @max_price = if params[:max_price] == ""
+        99999999999999
+      else
+        params[:max_price].to_i * 100
+      end
+      @size = params[:size]
+      if @size == ""
+        @items = Item.where(
+          "price > ? AND price < ? AND category_id = ?",
+          @min_price, @max_price, @category_id
+        )
+      else
+        @items = Item.where(
+          "price > ? AND price < ? AND size = ? AND category_id = ?",
+          @min_price, @max_price, @size, @category_id
+        )
+      end
+    else
+      @category_id = Category.where(item_type: params[:category])[0].id
+      @items = Item.where(category_id: @category_id)
+    end
+
+    respond_to { |i| i.js }
   end
 
   def package_show
