481371a0d353b5f005085dd4ef507cf396de20ae
Martin Giannakopoulos <martingianna@gmail.com>
2017-04-17 23:16:42 -0400

    added routes that we will use

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,32 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # grid with package items
+  get "/package", to: "pages#package"
+
+  # returns .js to choose item of a certain category
+  post "/package/category", to: "items#category_index"
+
+  # returns .js to view item/brand details and change size
+  post "/package/item", to: "items#package_show"
+
+  # list of individual items
+  get "/items", to: "items#index"
+
+  # individual itembrand details (not inside package view)
+  get "/items/:id", to: "items#show"
+
+  # list of past orders
+  get "/order/index", to: "orders#index"
+
+  # view shopping cart
+  get "/orderitem", to: "orderitems#index"
+
+  # adding an item to your cart
+  post "/orderitem", to: "orderitems#create"
+
+  # editting and removing items from your cart
+  patch "/orderitem/:id", to: "orderitems#update"
+  delete "/orderitem/:id", to: "orderitems#destroy"
 end
