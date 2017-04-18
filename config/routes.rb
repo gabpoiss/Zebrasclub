6038103aee1f11f24b04884132bc04ee06bc86b9
Martin Giannakopoulos <martingianna@gmail.com>
2017-04-17 23:20:55 -0400

    added paths

--- config/routes.rb
+++ config/routes.rb
@@ -3,30 +3,30 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   # grid with package items
-  get "/package", to: "pages#package"
+  get "/package", to: "pages#package", as: "package"
 
   # returns .js to choose item of a certain category
-  post "/package/category", to: "items#category_index"
+  post "/package/category", to: "items#package_index", as: "package_items"
 
   # returns .js to view item/brand details and change size
-  post "/package/item", to: "items#package_show"
+  post "/package/item", to: "items#package_show", as: "package_item"
 
   # list of individual items
-  get "/items", to: "items#index"
+  get "/items", to: "items#index", as: "items"
 
   # individual itembrand details (not inside package view)
-  get "/items/:id", to: "items#show"
+  get "/items/:id", to: "items#show", as: "item"
 
   # list of past orders
-  get "/order/index", to: "orders#index"
+  get "/order/index", to: "orders#index", as: "orders"
 
   # view shopping cart
-  get "/orderitem", to: "orderitems#index"
+  get "/orderitem", to: "orderitems#index", as: "orderitems"
 
   # adding an item to your cart
-  post "/orderitem", to: "orderitems#create"
+  post "/orderitem", to: "orderitems#create", as: "orderitem"
 
   # editting and removing items from your cart
-  patch "/orderitem/:id", to: "orderitems#update"
-  delete "/orderitem/:id", to: "orderitems#destroy"
+  patch "/orderitem/:id", to: "orderitems#update", as: "orderitem"
+  delete "/orderitem/:id", to: "orderitems#destroy", as: "orderitem"
 end
