254b73cebd916df81cc96440974200069fc58aff
gabriel poissant <poissantgabriel@gmail.com>
2017-04-21 13:08:52 -0400

    removed the ugly flashes alert and notices

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  setTimeout(function() {
-    $('#flash > div').slideUp();
-  }, 2000);
-});
+// $(document).ready(function() {
+//   setTimeout(function() {
+//     $('#flash > div').slideUp();
+//   }, 2000);
+// });
