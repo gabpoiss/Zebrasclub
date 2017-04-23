4f9792af755f00b5d33450ec698670cc1d92dc1f
Martin Giannakopoulos <martingianna@gmail.com>
2017-04-22 20:23:31 -0400

    some refactoring

--- app/controllers/orderitems_controller.rb
+++ app/controllers/orderitems_controller.rb
@@ -13,95 +13,111 @@ class OrderitemsController < ApplicationController
 
   def update
     if params[:quantity_update]
-      if current_user
-        order_item = OrderItem.find(params[:id])
-        order_item.quantity = params[:quantity]
-        order_item.save
-        @order_items = OrderItem.joins(:order).where(cart: true).where("orders.user_id = ?", current_user.id)
-      else
-        session[:package_items].each do |i|
-          if i["item_id"] == params[:id].to_i
-            i["quantity"] = params[:quantity]
-          end
-        end
-      end
-      redirect_to orderitems_path
+      quantity_update
     elsif params[:size_update]
-      @default_item = Item.find(params[:id])
-      @category = @default_item.category.item_type
-      @item = Item.where(size: params[:size], brand: @default_item.brand, category: @default_item.category, price: @default_item.price)[0]
-      @has_size = true
-      @in_cart = false
-      if current_user
-        order_item = OrderItem.joins(:order).where(item_id: params[:id], package: true).where("orders.user_id = ?", current_user.id)[0]
-        order_item.update(item_id: @item.id, size: true)
-        order_item.save
-      else
-        session[:package_items].each do |i|
-          if i["item_id"] == @default_item.id && i["package"] == true
-            i["item_id"] = @item.id
-            i["size"] = true
-          end
-        end
-      end
-      render "items/package_show"
+      size_update
     elsif params[:remove_from_package]
-      @criteria_for_extermination = Item.find(params[:id]).category.item_type
-      if current_user
-        order_item = OrderItem.joins(:order).where(item_id: params[:id], package: true).where("orders.user_id = ?", current_user.id)[0]
-        order_item.update(package: false)
-        order_item.save
-      else
-        session[:package_items].delete_if { |i| i["item_id"] == params[:id].to_i }
-      end
-      @items = []
-      @categories = Category.all
-
-      order_items = if current_user
-        User.find(current_user.id).order_items.where(package: true)
-      else
-        session[:package_items] ? session[:package_items] : []
-      end
-
-      @items = Item.where(id: order_items.map { |order_item| order_item["item_id"] })
-
-      @ready_to_order_package = false
-
-      if current_user
-        order_items = current_user.order_items.where(package: true)
-        @ready_to_order_package = order_items.all? { |i| i.cart }
-      else
-        @ready_to_order_package = session[:package_items].all? { |i| i["cart"]}
-      end
-
-      render "pages/package_main"
+      remove_from_package
     else
-      if current_user
-        order_item = OrderItem.joins(:order).where(item_id: params[:id], package: true).where("orders.user_id = ?", current_user.id)
-        order_item[0].cart = true
-        order_item[0].save
-      else
-        session[:package_items].each do |i|
-          if i["item_id"] == params[:id].to_i
-            i["cart"] = true
-          end
+      add_to_cart
+    end
+  end
+
+  def quantity_update
+    if current_user
+      order_item = OrderItem.find(params[:id])
+      order_item.quantity = params[:quantity]
+      order_item.save
+      @order_items = OrderItem.joins(:order).where(cart: true).where("orders.user_id = ?", current_user.id)
+    else
+      session[:package_items].each do |i|
+        if i["item_id"] == params[:id].to_i
+          i["quantity"] = params[:quantity]
         end
       end
-      @has_size = true
-      @in_cart = true
-      @item = Item.find(params[:id])
-      @category = @item.category.item_type
-
-      @ready_to_order_package = false
-
-      if current_user
-        order_items = current_user.order_items.where(package: true)
-        @ready_to_order_package = order_items.all? { |i| i.cart }
-      else
-        @ready_to_order_package = session[:package_items].all? { |i| i["cart"]}
-      end
-      render "items/package_show"
     end
+    redirect_to orderitems_path
+  end
+
+  def size_update
+    @default_item = Item.find(params[:id])
+    @category = @default_item.category.item_type
+    @item = Item.where(size: params[:size], brand: @default_item.brand, category: @default_item.category, price: @default_item.price)[0]
+    @has_size = true
+    @in_cart = false
+    if current_user
+      order_item = OrderItem.joins(:order).where(item_id: params[:id], package: true).where("orders.user_id = ?", current_user.id)[0]
+      order_item.update(item_id: @item.id, size: true)
+      order_item.save
+    else
+      session[:package_items].each do |i|
+        if i["item_id"] == @default_item.id && i["package"] == true
+          i["item_id"] = @item.id
+          i["size"] = true
+        end
+      end
+    end
+    render "items/package_show"
+  end
+
+  def remove_from_package
+    @criteria_for_elimination = Item.find(params[:id]).category.item_type
+    if current_user
+      order_item = OrderItem.joins(:order).where(item_id: params[:id], package: true).where("orders.user_id = ?", current_user.id)[0]
+      order_item.update(package: false)
+      order_item.save
+    else
+      session[:package_items].delete_if { |i| i["item_id"] == params[:id].to_i }
+    end
+    @items = []
+    @categories = Category.all
+
+    order_items = if current_user
+      User.find(current_user.id).order_items.where(package: true)
+    else
+      session[:package_items] ? session[:package_items] : []
+    end
+
+    @items = Item.where(id: order_items.map { |order_item| order_item["item_id"] })
+
+    @ready_to_order_package = false
+
+    if current_user
+      order_items = current_user.order_items.where(package: true)
+      @ready_to_order_package = order_items.all? { |i| i.cart }
+    else
+      @ready_to_order_package = session[:package_items].all? { |i| i["cart"]}
+    end
+
+    render "pages/package_main"
+  end
+
+  def add_to_cart
+    if current_user
+      order_item = OrderItem.joins(:order).where(item_id: params[:id], package: true).where("orders.user_id = ?", current_user.id)
+      order_item[0].cart = true
+      order_item[0].save
+    else
+      session[:package_items].each do |i|
+        if i["item_id"] == params[:id].to_i
+          i["cart"] = true
+        end
+      end
+    end
+    @has_size = true
+    @in_cart = true
+    @item = Item.find(params[:id])
+    @category = @item.category.item_type
+
+    @ready_to_order_package = false
+
+    if current_user
+      order_items = current_user.order_items.where(package: true)
+      @ready_to_order_package = order_items.all? { |i| i.cart }
+    else
+      @ready_to_order_package = session[:package_items].all? { |i| i["cart"]}
+    end
+    render "items/package_show"
   end
 
   def destroy
