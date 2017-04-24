4cb4527c8c5fb8f90089f1fd09bdf90ff032e40c
Martin Giannakopoulos <martingianna@gmail.com>
2017-04-24 12:04:18 -0400

    cart items transfer over upon sign-in and sign-up (albeit somewhat destructively).

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations',
+      omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
 
   root to: 'pages#home'
 
