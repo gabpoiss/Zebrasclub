f9ad4673f828141a442c443e1e71a685bdb4a77f
gabriel poissant <poissantgabriel@gmail.com>
2017-06-12 16:58:33 -0400

    Made available the individual items page. Messed around with the session_store. I have some code commented that permit you to store more then 4Kb cookie. But i wont use it for now. Added the link_to items_path. Added some card class for the items to look better.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,18 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_zebrasclub_session'
+
+
+
+
+# If we want to store more data into a session.
+
+# To make the :active_record_store functionality works in Rails 4/5, you must add the activerecord-session_store gem to your Gemfile:
+
+# gem 'activerecord-session_store'
+# then run the migration generator:
+
+# rails generate active_record:session_migration
+# And finally set your session store in config/initializers/session_store.rb:
+
+# Rails.application.config.session_store :active_record_store, :key => '_my_app_session'
